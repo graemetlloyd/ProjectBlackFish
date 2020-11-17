d1b4eb67756735f06a72545c6157929e92ba2f5f
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-11-17 11:43:01 +0000

    Adding character weighst to metatree script output.

--- RScripts/BuildMetatree.R
+++ RScripts/BuildMetatree.R
@@ -3,10 +3,9 @@
 # Load metatree library:
 library(metatree)
 
-# Set directory paths (Spencer, you will want to edit these for your own local version):
+# Set directory paths:
 MRPDirectory <- "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Input data/MRP"
 XMLDirectory <- "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Input data/XML"
-MetatreeDirectory <- ""
 
 # Standard exclusive data list (supertrees and the like):
 ExclusiveDataList <- c("Averianov_2016a", "Bravo_et_Gaete_2015a", "Brocklehurst_etal_2013a", "Brocklehurst_etal_2015aa", "Brocklehurst_etal_2015ab", "Brocklehurst_etal_2015ac", "Brocklehurst_etal_2015ad", "Brocklehurst_etal_2015ae", "Brocklehurst_etal_2015af", "Bronzati_etal_2012a", "Bronzati_etal_2015ab", "Brusatte_etal_2009ba", "Campbell_etal_2016ab", "Carr_et_Williamson_2004a", "Carr_etal_2017ab", "Frederickson_et_Tumarkin-Deratzian_2014aa", "Frederickson_et_Tumarkin-Deratzian_2014ab", "Frederickson_et_Tumarkin-Deratzian_2014ac", "Frederickson_et_Tumarkin-Deratzian_2014ad", "Garcia_etal_2006a", "Gatesy_etal_2004ab", "Grellet-Tinner_2006a", "Grellet-Tinner_et_Chiappe_2004a", "Grellet-Tinner_et_Makovicky_2006a", "Jin_etal_2010a", "Knoll_2008a", "Kurochkin_1996a", "Lopez-Martinez_et_Vicens_2012a", "Lu_etal_2014aa", "Norden_etal_2018a", "Pisani_etal_2002a", "Ruiz-Omenaca_etal_1997a", "Ruta_etal_2003ba", "Ruta_etal_2003bb", "Ruta_etal_2007a", "Schaeffer_etal_inpressa", "Selles_et_Galobart_2016a", "Sereno_1993a", "Sidor_2001a","Sidor_2003a", "Skutschas_etal_2019a", "Tanaka_etal_2011a", "Toljagic_et_Butler_2013a", "Tsuihiji_etal_2011aa", "Varricchio_et_Jackson_2004a", "Vila_etal_2017a", "Wilson_2005aa", "Wilson_2005ab", "Zelenitsky_et_Therrien_2008a")
@@ -54,16 +53,19 @@ CetaceaSafe$FullMRPMatrix$matrix_1$ordering <- c(CetaceaSafe$FullMRPMatrix$matri
 CetaceaSafe$FullMRPMatrix$matrix_1$character_weights <- c(CetaceaSafe$FullMRPMatrix$matrix_1$character_weights, McGowenSafe$FullMRPMatrix$matrix_1$character_weights)
 CetaceaSafe$FullMRPMatrix$matrix_1$minimum_values <- c(CetaceaSafe$FullMRPMatrix$matrix_1$minimum_values, McGowenSafe$FullMRPMatrix$matrix_1$minimum_values)
 CetaceaSafe$FullMRPMatrix$matrix_1$maximum_values <- c(CetaceaSafe$FullMRPMatrix$matrix_1$maximum_values, McGowenSafe$FullMRPMatrix$matrix_1$maximum_values)
+CetaceaSafe$CharacterWeights <- rbind(CetaceaSafe$CharacterWeights, matrix(data = c((nrow(CetaceaSafe$CharacterWeights) + 1):ncol(CetaceaSafe$FullMRPMatrix$matrix_1$matrix), CetaceaSafe$FullMRPMatrix$matrix_1$character_weights[(nrow(CetaceaSafe$CharacterWeights) + 1):ncol(CetaceaSafe$FullMRPMatrix$matrix_1$matrix)]), ncol = 2, dimnames = list(rep("McGowenSafe", times = ncol(CetaceaSafe$FullMRPMatrix$matrix_1$matrix) - nrow(CetaceaSafe$CharacterWeights)), c("Character", "Weight"))))
 CetaceaRisky$FullMRPMatrix$matrix_1$matrix <- cbind(CetaceaRisky$FullMRPMatrix$matrix_1$matrix, McGowenRisky$FullMRPMatrix$matrix_1$matrix[rownames(CetaceaRisky$FullMRPMatrix$matrix_1$matrix), ])
 CetaceaRisky$FullMRPMatrix$matrix_1$ordering <- c(CetaceaRisky$FullMRPMatrix$matrix_1$ordering, McGowenRisky$FullMRPMatrix$matrix_1$ordering)
 CetaceaRisky$FullMRPMatrix$matrix_1$character_weights <- c(CetaceaRisky$FullMRPMatrix$matrix_1$character_weights, McGowenRisky$FullMRPMatrix$matrix_1$character_weights)
 CetaceaRisky$FullMRPMatrix$matrix_1$minimum_values <- c(CetaceaRisky$FullMRPMatrix$matrix_1$minimum_values, McGowenRisky$FullMRPMatrix$matrix_1$minimum_values)
 CetaceaRisky$FullMRPMatrix$matrix_1$maximum_values <- c(CetaceaRisky$FullMRPMatrix$matrix_1$maximum_values, McGowenRisky$FullMRPMatrix$matrix_1$maximum_values)
+CetaceaRisky$CharacterWeights <- rbind(CetaceaRisky$CharacterWeights, matrix(data = c((nrow(CetaceaRisky$CharacterWeights) + 1):ncol(CetaceaRisky$FullMRPMatrix$matrix_1$matrix), CetaceaRisky$FullMRPMatrix$matrix_1$character_weights[(nrow(CetaceaRisky$CharacterWeights) + 1):ncol(CetaceaRisky$FullMRPMatrix$matrix_1$matrix)]), ncol = 2, dimnames = list(rep("McGowenRisky", times = ncol(CetaceaRisky$FullMRPMatrix$matrix_1$matrix) - nrow(CetaceaRisky$CharacterWeights)), c("Character", "Weight"))))
 CetaceaDangerous$FullMRPMatrix$matrix_1$matrix <- cbind(CetaceaDangerous$FullMRPMatrix$matrix_1$matrix, McGowenDangerous$FullMRPMatrix$matrix_1$matrix[rownames(CetaceaDangerous$FullMRPMatrix$matrix_1$matrix), ])
 CetaceaDangerous$FullMRPMatrix$matrix_1$ordering <- c(CetaceaDangerous$FullMRPMatrix$matrix_1$ordering, McGowenDangerous$FullMRPMatrix$matrix_1$ordering)
 CetaceaDangerous$FullMRPMatrix$matrix_1$character_weights <- c(CetaceaDangerous$FullMRPMatrix$matrix_1$character_weights, McGowenDangerous$FullMRPMatrix$matrix_1$character_weights)
 CetaceaDangerous$FullMRPMatrix$matrix_1$minimum_values <- c(CetaceaDangerous$FullMRPMatrix$matrix_1$minimum_values, McGowenDangerous$FullMRPMatrix$matrix_1$minimum_values)
 CetaceaDangerous$FullMRPMatrix$matrix_1$maximum_values <- c(CetaceaDangerous$FullMRPMatrix$matrix_1$maximum_values, McGowenDangerous$FullMRPMatrix$matrix_1$maximum_values)
+CetaceaDangerous$CharacterWeights <- rbind(CetaceaDangerous$CharacterWeights, matrix(data = c((nrow(CetaceaDangerous$CharacterWeights) + 1):ncol(CetaceaDangerous$FullMRPMatrix$matrix_1$matrix), CetaceaDangerous$FullMRPMatrix$matrix_1$character_weights[(nrow(CetaceaDangerous$CharacterWeights) + 1):ncol(CetaceaDangerous$FullMRPMatrix$matrix_1$matrix)]), ncol = 2, dimnames = list(rep("McGowenDangerous", times = ncol(CetaceaDangerous$FullMRPMatrix$matrix_1$matrix) - nrow(CetaceaDangerous$CharacterWeights)), c("Character", "Weight"))))
 
 # Perform STR:
 SafeSTR <- Claddis::safe_taxonomic_reduction(CetaceaSafe$FullMRPMatrix)
@@ -104,6 +106,7 @@ Claddis::write_nexus_matrix(CetaceaSafe$STRMRPMatrix, "~/Dropbox/Mammal_Supertre
 Claddis::write_tnt_matrix(CetaceaSafe$FullMRPMatrix, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Safe/FULL.tnt")
 Claddis::write_tnt_matrix(CetaceaSafe$STRMRPMatrix, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Safe/STR.tnt")
 write.table(CetaceaSafe$SafelyRemovedTaxa, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Safe/STR.txt", row.names = FALSE)
+write.table(CetaceaSafe$CharacterWeights, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Safe/CharacterWeights.txt", row.names = TRUE)
 
 # Write out risky metatree files:
 Claddis::write_nexus_matrix(CetaceaRisky$FullMRPMatrix, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Risky/FULL.nex")
@@ -111,6 +114,7 @@ Claddis::write_nexus_matrix(CetaceaRisky$STRMRPMatrix, "~/Dropbox/Mammal_Supertr
 Claddis::write_tnt_matrix(CetaceaRisky$FullMRPMatrix, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Risky/FULL.tnt")
 Claddis::write_tnt_matrix(CetaceaRisky$STRMRPMatrix, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Risky/STR.tnt")
 write.table(CetaceaRisky$SafelyRemovedTaxa, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Risky/STR.txt", row.names = FALSE)
+write.table(CetaceaRisky$CharacterWeights, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Risky/CharacterWeights.txt", row.names = TRUE)
 
 # Write out dangerous metatree files:
 Claddis::write_nexus_matrix(CetaceaDangerous$FullMRPMatrix, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Dangerous/FULL.nex")
@@ -118,6 +122,7 @@ Claddis::write_nexus_matrix(CetaceaDangerous$STRMRPMatrix, "~/Dropbox/Mammal_Sup
 Claddis::write_tnt_matrix(CetaceaDangerous$FullMRPMatrix, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Dangerous/FULL.tnt")
 Claddis::write_tnt_matrix(CetaceaDangerous$STRMRPMatrix, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Dangerous/STR.tnt")
 write.table(CetaceaDangerous$SafelyRemovedTaxa, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Dangerous/STR.txt", row.names = FALSE)
+write.table(CetaceaDangerous$CharacterWeights, "~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Dangerous/CharacterWeights.txt", row.names = TRUE)
 
 # Add new analysis block to exclude TNT:
 SafeSTRTNT <- readLines("~/Dropbox/Mammal_Supertree/ProjectBlackFish/Metatree data/Safe/STR.tnt")
